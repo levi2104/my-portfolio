eb95a6aa103be8d818796eabb04516e6ec19e105
Aryan Tanna <116259512+levi2104@users.noreply.github.com>
2025-02-25 12:29:46 +0530

    Update Hero.jsx

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -33,34 +33,32 @@ const Hero = () => {
         transition={{ duration: 1 }}
         className="text-center md:text-left md:absolute md:left-10 md:top-1/3 z-10"
       >
-        <div className="w-screen h-screen">
-          <h1 className="text-4xl md:text-5xl font-bold text-neonBlue">
-            Aryan Tanna
-          </h1>
-          <h2 className="mt-3 text-lg md:text-xl text-neonPurple">
-            <TypingEffect />
-          </h2>
-          <p className="mt-4 md:mt-6 text-sm md:text-md">
-            Creating stunning, high-performance web applications.
-          </p>
-  
-          <motion.div
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1 }}
-            className="mt-6 md:mt-9"
-          >
-            <Link to="contact" smooth={true} duration={800}>
-              <span className="px-5 py-3 w-fit bg-neonPurple text-white font-semibold rounded-lg shadow-lg hover:scale-105 transition-all duration-300 block cursor-pointer lg:w-[26%] lg:mx-0 mx-auto">
-                Hire Me
-              </span>
-            </Link>
-          </motion.div>
-        </div>
+        <h1 className="text-4xl md:text-5xl font-bold text-neonBlue">
+          Aryan Tanna
+        </h1>
+        <h2 className="mt-3 text-lg md:text-xl text-neonPurple">
+          <TypingEffect />
+        </h2>
+        <p className="mt-4 md:mt-6 text-sm md:text-md">
+          Creating stunning, high-performance web applications.
+        </p>
+
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="mt-6 md:mt-9"
+        >
+          <Link to="contact" smooth={true} duration={800}>
+            <span className="px-5 py-3 w-fit bg-neonPurple text-white font-semibold rounded-lg shadow-lg hover:scale-105 transition-all duration-300 block cursor-pointer lg:w-[26%] lg:mx-0 mx-auto">
+              Hire Me
+            </span>
+          </Link>
+        </motion.div>
       </motion.div>
 
       {/* 3D Model */}
-      <div className="absolute inset-0 w-full h-full flex justify-center items-center">
+      <div className="absolute inset-0 w-full h-full flex justify-center items-center pointer-events-none">
         <Canvas className="w-full h-full">
           <OrbitControls enableZoom={false} enablePan={false} enabled={false} />
           <ambientLight intensity={0.5} />
